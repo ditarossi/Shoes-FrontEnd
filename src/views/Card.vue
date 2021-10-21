28fdf5d6e9b826298294f581a5fe5b3823df15ba
Dita Rossiyana <ditarossiyana12@gmail.com>
2021-10-21 12:42:51 +0700

    card

--- src/views/Card.vue
+++ src/views/Card.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="card" style="width: 18rem;">
+<div class="row">
+    <div class="col">
+        <div class="card border-0" style="width: 18rem;">
         <img src="../assets/image/spt3.jpg" class="produk">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-sd">Show Details</a>
+            <p class="card-text">Rp. 250.000,-</p>
+            <a href="#" class="btn btn-dark">Show Details</a>
         </div>
     </div>
+    </div>
+</div>
+    
 </template>
 
 <script lang="ts">
